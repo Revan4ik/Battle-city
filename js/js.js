dc03e9ca60b7acb6d0894f8781b312777c661f59
Revan4ik <gaetndyuude@gmail.com>
2022-05-29 22:35:06 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,6 +2,7 @@ var step = 10;
 document.addEventListener('keydown', function (e){
     var car = document.querySelector('.Tank');
     var bullet = document.querySelector('.bullet');
+    var fire = document.querySelector('.fire')
     var hCar = car.getBoundingClientRect().height;
     var wCar = car.getBoundingClientRect().width;
     var lCar = car.getBoundingClientRect().left;
@@ -12,6 +13,7 @@ document.addEventListener('keydown', function (e){
             : lCar + step + 'px'
         car.style.transform = 'rotate(180deg)';
         bullet.style.cssText = "display: hidden"
+        fire.style.cssText = "display: hidden"
         bullet.style.width = '20px'
     } else if(e.code === "KeyA"){
         car.style.left = lCar < 0 
@@ -19,6 +21,7 @@ document.addEventListener('keydown', function (e){
         : lCar - step + 'px'
         car.style.transform = 'rotate(0deg)';
         bullet.style.cssText = "display: hidden"
+        fire.style.cssText = "display: hidden"
         bullet.style.width = '20.1px'
     } else if (e.code === "KeyS"){
       car.style.transform = 'rotate(270deg)'
@@ -26,6 +29,7 @@ document.addEventListener('keydown', function (e){
       ? window.innerHeight - hCar + 'px'
       : tCar + step + 'px'
       bullet.style.cssText = "display: hidden"
+      fire.style.cssText = "display: hidden"
       bullet.style.width = '20.2px'
 
     }
@@ -34,27 +38,43 @@ document.addEventListener('keydown', function (e){
       car.style.top = tCar < 0
       ? 0 + 'px'
       : tCar - step + 'px'
+      fire.style.cssText = "display: hidden"
       bullet.style.cssText = "display: hidden"
       bullet.style.width = '20.3px'
     } else if (e.code === "Space"){
       if (bullet.style.width == 20 + 'px'){
+      fire.style.cssText = "display: block;"
+      fire.style.left = lCar + wCar + 'px';
+      fire.style.top = hCar / 2 + tCar -4 + 'px';
       bullet.style.cssText = "display: block;"
-      bullet.style.left = lCar + wCar + 'px';
+      bullet.style.left = lCar + wCar + 30 + 'px';
       bullet.style.top = hCar / 2 + tCar -4 + 'px';
+      fire.style.transform = 'rotate(90deg)';
       } else if (bullet.style.width == 20.1 + 'px'){
+      fire.style.cssText = "display: block;"
+      fire.style.left = lCar + wCar - 110 + 'px';
+      fire.style.top = hCar + tCar - 56 + 'px';
       bullet.style.cssText = "display: block;"
-      bullet.style.left = lCar + wCar - 122 + 'px';
+      bullet.style.left = lCar + wCar - 150 + 'px';
       bullet.style.top = hCar + tCar - 56 + 'px';
       bullet.style.transform = 'rotate(180deg)';
+      fire.style.transform = 'rotate(270deg)';
       } else if (bullet.style.width == 20.2 + 'px'){
+        fire.style.cssText = "display: block;"
+        fire.style.left = lCar + wCar - 57 + 'px';
+        fire.style.top = hCar + tCar + 1 + 'px';
         bullet.style.cssText = "display: block;"
         bullet.style.left = lCar + wCar - 61 + 'px';
-        bullet.style.top = hCar + tCar + 7 + 'px';
+        bullet.style.top = hCar + tCar + 35 + 'px';
         bullet.style.transform = 'rotate(90deg)';
+        fire.style.transform = 'rotate(180deg)';
         } else if (bullet.style.width == 20.3 + 'px'){
+          fire.style.cssText = "display: block;"
+          fire.style.left = lCar + wCar / 2 - 4 + 'px';
+          fire.style.top =  tCar - 10 + 'px';
           bullet.style.cssText = "display: block;"
           bullet.style.left = lCar + wCar / 2 - 8 + 'px';
-          bullet.style.top =  tCar - 15 + 'px';
+          bullet.style.top =  tCar - 50 + 'px';
           bullet.style.transform = 'rotate(270deg)';
           }
     }
